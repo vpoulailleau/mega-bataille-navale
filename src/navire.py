441407acb694b8e987ed07947851e2829c60cd38
DvAx <devaux25@gmail.com>
2020-01-24 15:55:07 +0100

    add case list in Navire class

--- src/navire.py
+++ src/navire.py
@@ -1,12 +1,13 @@
-class navire:
+class Navire:
     def __init__(self, longueur, largeur, nom):
         self.longueur = longueur
         self.largeur  = largeur
         self.nom      = nom
+        self.cases     = []
 
-    def test_impact(self):
+    def test_impact(self, x, y, z):
         pass
 
-    def set_position(self):
+    def set_position(self, x, y, z):
         pass
 
