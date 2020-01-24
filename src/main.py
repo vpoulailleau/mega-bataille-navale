5b7d0fb87adfcc9e03e87d28aadd3288df506a4d
LavergneC <lavergne.clement@yahoo.fr>
2020-01-24 15:54:38 +0100

    First python-QML link

--- src/main.py
+++ src/main.py
@@ -1,15 +1,13 @@
 import sys
+from PyQt5.QtQml import QQmlApplicationEngine
 
 from PySide2.QtWidgets import QApplication, QLabel
-
                                                      
 
 if __name__ == "__main__":
 
     app = QApplication(sys.argv)
-
-    label = QLabel("Hello World")
-
-    label.show()
+    engine = QQmlApplicationEngine()
+    engine.load("src/qml/main.qml")
 
     sys.exit(app.exec_())
---
+++ src/qml/main.qml
@@ -0,0 +1,13 @@
+import QtQuick 2.6
+import QtQuick.Controls 2.1
+
+ApplicationWindow {
+    width: 540
+    height: 300
+    visible: true
+
+    Button{
+        text: "coucou"
+    }
+
+}
