9566c0c9d61debeb21f7c3ef9beb7c88f2230d86
nicolas_gibaud <n.gibaud@catie.fr>
2020-01-24 16:24:58 +0100

    Add Jeu class

---
+++ src/jeu.py
@@ -0,0 +1,13 @@
+from Carte import *
+
+class Jeu:
+    
+    def __init__(self):
+        self.carte_perso = Carte()
+        self.carte_adversaire = Carte()
+    
+    def recevoir_tir(self, x, y):
+        pass
+    
+    def placer_navire(self, x, y, z):
+        pass
