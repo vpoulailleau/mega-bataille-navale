501aa9bfec7c2b8a327438710dbdcd8df3940095
DvAx <devaux25@gmail.com>
2020-01-30 17:00:10 +0100

    suppression recevoir_tir en trop

--- src/jeu.py
+++ src/jeu.py
@@ -1,13 +1,10 @@
-from Carte import *
+from carte import *
 
 class Jeu:
     
     def __init__(self):
         self.carte_perso = Carte()
         self.carte_adversaire = Carte()
-    
-    def recevoir_tir(self, x, y):
-        pass
     
     def placer_navire(self, x, y, z, sens, name):
         pass
