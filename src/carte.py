f53d80b8e7fd4e14095849b1f45f5a625d94782b
nicolas_gibaud <n.gibaud@catie.fr>
2020-01-30 18:40:26 +0100

    Ajout index case + actualisation etat case

--- src/carte.py
+++ src/carte.py
@@ -3,12 +3,17 @@ from navire import *
 class Carte:
     def __init__(self):
         self.navires = []
+        self.cases = []
 
     def check_ship(self, x, y, z):
         for navire in self.navires:
-            if navire.test_impact(x, y, z):
+            case_touche, etat_attaque = navire.test_impact(x, y, z)
+            if etat_attaque:
+                for index, case in enumerate(self.cases):
+                    if case.index == case_touche.index:
+                       self.cases[index].impact = True
                 return True
         return False
-        
+
     def positionner_navire(self, x, y, z, sens, name):
         pass
