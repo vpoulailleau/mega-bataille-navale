e58f9b77ae27431bd540f3dd4a0d90633d6581aa
DvAx <devaux25@gmail.com>
2020-01-30 16:58:39 +0100

    implementation check_ship

--- src/carte.py
+++ src/carte.py
@@ -1,9 +1,15 @@
+from navire import *
+
 class Carte:
     def __init__(self):
         self.navires = []
 
     def check_ship(self, x, y, z):
-        pass
+        for navire in self.navires:
+            if navire.test_impact(x, y, z):
+                return True
+        return False
+
 
     def positionner_navire(self, x, y, z, sens, name):
         pass
