16fe72b857f768808174858709debcfa115939f4
DvAx <devaux25@gmail.com>
2020-01-30 13:02:13 +0100

    parse message pour tirs

--- src/jeu.py
+++ src/jeu.py
@@ -11,3 +11,14 @@ class Jeu:
     
     def placer_navire(self, x, y, z, sens, name):
         pass
+
+    def recevoir_tir(self, x, y):
+        pass
+
+    def parse_message(self, trame):
+        if trame[0] == 2:
+            # Reception d'un tir
+            x = trame[1]
+            y = trame[2]
+            return (x,y)
+            
