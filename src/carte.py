61936b8e4cbcb501251fb0352ba2bdfed21a97da
nicolas_gibaud <n.gibaud@catie.fr>
2020-01-31 12:32:55 +0100

    Initialisation des cases dans le constructeur de Carte

--- src/carte.py
+++ src/carte.py
@@ -3,12 +3,23 @@ from navire import *
 class Carte:
     def __init__(self):
         self.navires = []
+        self.cases = []
+        z = 0
+        while z < 3:
+            y = 0
+            while y < 15:
+                x = 0
+                while x < 15:
+                    self.cases.append(Case(x, y, z))
+                    x += 1
+                y += 1
+            z += 1
 
     def check_ship(self, x, y, z):
         for navire in self.navires:
             if navire.test_impact(x, y, z):
                 return True
         return False
-        
+
     def positionner_navire(self, x, y, z, sens, name):
         pass
